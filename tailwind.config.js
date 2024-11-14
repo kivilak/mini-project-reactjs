940647f3d2ee6b965b7552ac20de317e3a54c230
KivilakChathu <kivilakc01@gmail.com>
2024-11-14 13:51:47 +0530

    feat: add navigation bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'lobster': ['Lobster', 'sans-serif'],
+      'quicksand': ['Quicksand', 'sans-serif'],
+    },
+    fontSize: {
+      'header-text': '6vw',
+    },
+    extend: {
+      backgroundImage: {
+        'header': "url('./src/assets/header.webp')"
+      },
+      colors: {
+        'baker-yellow': '#F4B411',
+        'text-color': '#FFFAE7',
+        'nav-active': '#9B7104',
+      }
+    },
   },
   plugins: [],
 }
