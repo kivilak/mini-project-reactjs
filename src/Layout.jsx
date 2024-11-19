40bba412cc273c17016b38c2ebe2ed0b2a84d4bd
KivilakCha <kivilakc01@gmail.com>
2024-11-19 14:35:46 +0530

    feat
    Add footer to the website

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function Layout() {
-    return(
+    return (
         <>
             <Header />
             <Outlet />
+            <Footer />
         </>
     )
 }
